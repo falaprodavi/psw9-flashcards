3498786c694a2222f098c047654294fa81e1a1e9
falaprodavi <agencianatural@gmail.com>
2024-01-20 18:05:08 -0300

    urls update

--- study_async/settings.py
+++ study_async/settings.py
@@ -135,11 +135,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-#STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 # Default primary key field type
--- study_async/urls.py
+++ study_async/urls.py
@@ -11,6 +11,3 @@ urlpatterns = [
     path('flashcard/', include('flashcard.urls')),
     path('apostilas/', include('apostilas.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
