873d194468c02d9dd10c9a7da7fe88809d27cb93
falaprodavi <agencianatural@gmail.com>
2024-01-20 17:50:40 -0300

    staticfiles_build

--- study_async/settings.py
+++ study_async/settings.py
@@ -135,9 +135,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+#STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
